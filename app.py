527eaa4f6e79ac3fd118775d2ad1bea11361f3e1
Alan <alan.steinbarth@gmail.com>
2025-04-22 15:24:36 +0200

    wracam do pierwotnej wersji

--- app.py
+++ app.py
@@ -3,41 +3,52 @@ import streamlit as st
 from audiorecorder import audiorecorder  # type: ignore
 from dotenv import dotenv_values
 from hashlib import md5
-import openai
+from openai import OpenAI
 from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct, Distance, VectorParams
 
 
 env = dotenv_values(".env")
 ### Secrets using Streamlit Cloud Mechanism
+# https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management
 if 'QDRANT_URL' in st.secrets:
-    env['QDRANT_URL'] = st.secrets["QDRANT_URL"]
+    env['QDRANT_URL'] = st.secrets['QDRANT_URL']
 if 'QDRANT_API_KEY' in st.secrets:
-    env['QDRANT_API_KEY'] = st.secrets["QDRANT_API_KEY"]
+    env['QDRANT_API_KEY'] = st.secrets['QDRANT_API_KEY']
+###
 
 EMBEDDING_MODEL = "text-embedding-3-large"
+
 EMBEDDING_DIM = 3072
+
 AUDIO_TRANSCRIBE_MODEL = "whisper-1"
+
 QDRANT_COLLECTION_NAME = "notes"
 
+def get_openai_client():
+    return OpenAI(api_key=st.session_state["openai_api_key"])
+
 def transcribe_audio(audio_bytes):
+    openai_client = get_openai_client()
     audio_file = BytesIO(audio_bytes)
     audio_file.name = "audio.mp3"
-
-    transcript = openai.audio.transcribe(
-        model=AUDIO_TRANSCRIBE_MODEL,
+    transcript = openai_client.audio.transcriptions.create(
         file=audio_file,
-        response_format="text"
+        model=AUDIO_TRANSCRIBE_MODEL,
+        response_format="verbose_json",
     )
 
-    return transcript
+    return transcript.text
 
+#
+# DB
+#
 @st.cache_resource
 def get_qdrant_client():
     return QdrantClient(
-        url=env["QDRANT_URL"],
-        api_key=env["QDRANT_API_KEY"],
-    )
+    url=env["QDRANT_URL"], 
+    api_key=env["QDRANT_API_KEY"],
+)
 
 def assure_db_collection_exists():
     qdrant_client = get_qdrant_client()
@@ -53,8 +64,9 @@ def assure_db_collection_exists():
     else:
         print("Kolekcja już istnieje")
 
-def get_embedding(text):
-    result = openai.embeddings.create(
+def get_embeddings(text):
+    openai_client = get_openai_client()
+    result = openai_client.embeddings.create(
         input=[text],
         model=EMBEDDING_MODEL,
         dimensions=EMBEDDING_DIM,
@@ -73,8 +85,10 @@ def add_note_to_db(note_text):
         points=[
             PointStruct(
                 id=points_count.count + 1,
-                vector=get_embedding(text=note_text),
-                payload={"text": note_text},
+                vector=get_embeddings(text=note_text),
+                payload={
+                    "text": note_text,
+                },
             )
         ]
     )
@@ -83,25 +97,41 @@ def list_notes_from_db(query=None):
     qdrant_client = get_qdrant_client()
     if not query:
         notes = qdrant_client.scroll(collection_name=QDRANT_COLLECTION_NAME, limit=10)[0]
-        return [{"text": note.payload["text"], "score": None} for note in notes]
+        result = []
+        for note in notes:
+            result.append({
+                "text": note.payload["text"],
+                "score": None,
+            })
+
+        return result
+
     else:
         notes = qdrant_client.search(
             collection_name=QDRANT_COLLECTION_NAME,
-            query_vector=get_embedding(text=query),
+            query_vector= get_embeddings(text=query),
             limit=10,
         )
-        return [{"text": note.payload["text"], "score": note.score} for note in notes]
+        result = []
+        for note in notes:
+            result.append({
+                "text": note.payload["text"],
+                "score": note.score,
+            })
 
-# ----------------------
+        return result
+
+
+#
 # MAIN
-# ----------------------
-
+#
 st.set_page_config(page_title="Audio Notatki", layout="centered")
 
-# OpenAI API key
+# OpenAI API key protection
 if not st.session_state.get("openai_api_key"):
     if "OPENAI_API_KEY" in env:
         st.session_state["openai_api_key"] = env["OPENAI_API_KEY"]
+
     else:
         st.info("Dodaj swój klucz API OpenAI aby móc korzystać z tej aplikacji")
         st.session_state["openai_api_key"] = st.text_input("Klucz API", type="password")
@@ -111,30 +141,27 @@ if not st.session_state.get("openai_api_key"):
 if not st.session_state.get("openai_api_key"):
     st.stop()
 
-# Ustawienie klucza dla openai
-openai.api_key = st.session_state["openai_api_key"]
-
 # Session state initialization
 if "note_audio_bytes_md5" not in st.session_state:
     st.session_state["note_audio_bytes_md5"] = None
+
 if "note_audio_bytes" not in st.session_state:
     st.session_state["note_audio_bytes"] = None
+
 if "note_text" not in st.session_state:
     st.session_state["note_text"] = ""
+
 if "note_audio_text" not in st.session_state:
     st.session_state["note_audio_text"] = ""
 
 st.title("Audio Notatki")
 assure_db_collection_exists()
-
 add_tab, search_tab = st.tabs(["Dodaj notatkę", "Wyszukaj notatkę"])
-
 with add_tab:
     note_audio = audiorecorder(
         start_prompt="Nagraj notatkę",
         stop_prompt="Zatrzymaj nagrywanie",
     )
-
     if note_audio:
         audio = BytesIO()
         note_audio.export(audio, format="mp3")
@@ -154,6 +181,8 @@ with add_tab:
             st.session_state["note_text"] = st.text_area("Edytuj notatkę", value=st.session_state["note_audio_text"])
 
         if st.session_state["note_text"] and st.button("Zapisz notatkę", disabled=not st.session_state["note_text"]):
+            qdrant_client = get_qdrant_client()
+
             add_note_to_db(note_text=st.session_state["note_text"])
             st.toast("Notatka zapisana", icon="🎉")
 
